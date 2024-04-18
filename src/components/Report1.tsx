7803d346f3e5b8a9c2d7af593ef40024b1d70fb7
Skyrdow <lucas.mesias@usach.cl>
2024-04-17 21:51:23 -0400

    add: patent finder

--- src/components/Report1.tsx
+++ src/components/Report1.tsx
@@ -9,22 +9,51 @@ const Report1 = () => {
       setReport1(response.data);
     });
   }, []);
+
+  const [patent, setPatent] = useState("");
+  const handleInputChange = (
+    event: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>,
+  ) => {
+    const { value } = event.target;
+    setPatent(value);
+  };
+
   return (
-    <div className="bg-white p-5 rounded-xl shadow-xl">
-      <div className="flex w-full overflow-x-auto">
-        <table className="table-zebra bg-success/10 table">
-          <thead>
-            <tr>
-              <th>Patente</th>
-              <th>Precio total</th>
-              <th>Precio base</th>
-              <th>Descuentos</th>
-              <th>Recargos</th>
-              <th>Iva</th>
-            </tr>
-          </thead>
-          <tbody>
-            {report1.map((item: any) => (
+    <div className="bg-white p-5 rounded-xl shadow-xl flex flex-col items-center gap-4">
+      <div className="form-field">
+        <label className="form-label" htmlFor="patent">
+          Patente:
+        </label>
+        <input
+          className="input input-ghost-success"
+          type="text"
+          id="patent"
+          name="patent"
+          value={patent}
+          onChange={handleInputChange}
+          required
+        />
+      </div>
+      <table className="table-zebra bg-success/10 table">
+        <thead>
+          <tr>
+            <th>Patente</th>
+            <th>Precio total</th>
+            <th>Precio base</th>
+            <th>Descuentos</th>
+            <th>Recargos</th>
+            <th>Iva</th>
+          </tr>
+        </thead>
+        <tbody>
+          {
+            report1.filter((item: any) => {
+              if (patent === "") {
+                return true;
+              } else {
+                return item.car === patent;
+              }
+            }).map((item: any) => (
               <tr>
                 <th>{item.car}</th>
                 <td>{item.totalPrice.toLocaleString()}</td>
@@ -33,10 +62,10 @@ const Report1 = () => {
                 <td>{item.surcharges.toLocaleString()}</td>
                 <td>{item.iva.toLocaleString()}</td>
               </tr>
-            ))}
-          </tbody>
-        </table>
-      </div>
+            ))
+          }
+        </tbody>
+      </table>
     </div>
   );
 };
